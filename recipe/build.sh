4955f842cfcfdc266e8fbd488fa5ca1996010832
Ray Donnelly <mingw.android@gmail.com>
2017-08-27 17:27:43 +0100

    Generalize logging

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,12 @@ export CFLAGS="-fPIC $CFLAGS"
             --enable-static \
             --disable-rpath
 
-make -j$CPU_COUNT
-make check -j$CPU_COUNT
-make install -j$CPU_COUNT
+make -j${CPU_COUNT} ${VERBOSE_AT}
+make check
+make install
+
+if [[ $(uname) == Linux ]]; then
+  chmod 755 $PREFIX/lib/libiconv.so.2.5.1
+  chmod 755 $PREFIX/lib/libcharset.so.1.0.0
+  chmod 755 $PREFIX/lib/preloadable_libiconv.so
+fi
