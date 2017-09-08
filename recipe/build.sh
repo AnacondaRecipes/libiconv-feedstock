ba463cc78aacb2c5b11f43039f13bdf93e749464
Ray Donnelly <mingw.android@gmail.com>
2017-09-08 09:34:42 +0000

    Update to 1.15 and fix compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
-export CFLAGS="-fPIC $CFLAGS"
-
-./configure --prefix=${PREFIX} \
-            --host=${HOST} \
-            --enable-static \
+./configure --prefix=${PREFIX}  \
+            --host=${HOST}      \
+            --build=${BUILD}    \
+            --enable-static     \
             --disable-rpath
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
 
-if [[ $(uname) == Linux ]]; then
-  chmod 755 $PREFIX/lib/libiconv.so.2.5.1
-  chmod 755 $PREFIX/lib/libcharset.so.1.0.0
-  chmod 755 $PREFIX/lib/preloadable_libiconv.so
+# TODO :: Only provide a static iconv executable for GNU/Linux.
+# TODO :: glibc has iconv built-in. I am only providing it here
+# TODO :: for legacy packages (and through gritted teeth).
+if [[ ${HOST} =~ .*linux.* ]]; then
+  chmod 755 ${PREFIX}/lib/libiconv.so.2.6.0
+  chmod 755 ${PREFIX}/lib/libcharset.so.1.0.0
+  chmod 755 ${PREFIX}/lib/preloadable_libiconv.so
 fi
